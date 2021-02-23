0d40d5c03be1736c65ebc223b1f4cebf411fb567
William Sedlacek <wsedlacekc@gmail.com>
2021-02-23 15:46:14 -0700

    perf: synchronous updates
    This refactors `useTextWidth` to make use of `useMemo` so that updates are synchronous.
    Additionally this moves `getTextWidth` and `getContext` to the module scope so that they do not need to be redeclared on every effect call.

--- src/useTextWidth.tsx
+++ src/useTextWidth.tsx
@@ -1,44 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
 export interface useTextWidthType {
   ({ text, font, ref }: { text?: string | string[]; font?: string; ref?: any }): number;
 }
 
+const getContext = () => {
+  const fragment: DocumentFragment = document.createDocumentFragment();
+  const canvas: HTMLCanvasElement = document.createElement('canvas');
+  fragment.appendChild(canvas);
+  return canvas.getContext('2d');
+}
+
+const getTextWidth = (currentText: string | string[], font: string) => {
+  const context = getContext();
+  context.font = font;
+
+  if (Array.isArray(currentText)) {
+    return Math.max(...currentText.map((t) => context.measureText(t).width));
+  } else {
+    const metrics = context.measureText(currentText);
+    return metrics.width;
+  }
+}
+
 const useTextWidth: useTextWidthType = ({ text, ref, font = '16px Times' }) => {
-  const [width, setWidth] = useState(-1);
-
-  useEffect(() => {
-    const getContext = () => {
-      const fragment: DocumentFragment = document.createDocumentFragment();
-      const canvas: HTMLCanvasElement = document.createElement('canvas');
-      fragment.appendChild(canvas);
-      return canvas.getContext('2d');
-    };
-
-    const getTextWidth = (currentText: string | string[]) => {
-      const context = getContext();
-      context.font = font;
-
-      if (Array.isArray(currentText)) {
-        return Math.max(...currentText.map((t) => context.measureText(t).width));
-      } else {
-        const metrics = context.measureText(currentText);
-        return metrics.width;
-      }
-    };
-
+  return useMemo(() => {
     if (ref && ref.current) {
       const context = getContext();
       const computedStyles = window.getComputedStyle(ref.current);
       context.font = computedStyles.font;
       const metrics = context.measureText(ref.current.textContent);
-      setWidth(metrics.width);
+      return metrics.width;
     } else if (text) {
-      setWidth(getTextWidth(text));
+      return getTextWidth(text, font);
     }
   }, [font, ref, text]);
-
-  return width;
 };
 
 export default useTextWidth;
